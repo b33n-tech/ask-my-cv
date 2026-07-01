ed8963b06b5c0cf160497b1f88f0ba7d0f0052d7
b33n-tech <benjamin.griot@outlook.fr>
2026-07-01 10:12:21 +0200

    Update API logic

--- api/chat.js
+++ api/chat.js
@@ -1,7 +1,7 @@
 // api/chat.js
 export default async function handler(req, res) {
   // Configuration CORS pour autoriser votre GitHub Pages
-  res.setHeader('Access-Control-Allow-Origin', '*'); 
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
